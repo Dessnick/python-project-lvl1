df218ee9232d7d357e802be655e2c9a7131a204d
Dessnick <dessnick@gmail.com>
2020-05-25 13:00:50 +0700

    refactoring

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -2,4 +2,4 @@ import brain_games.games.calc
 import brain_games.games.even
 import brain_games.games.gcd
 import brain_games.games.prime
-import brain_games.games.progression
+import brain_games.games.progression  # noqa:F401
