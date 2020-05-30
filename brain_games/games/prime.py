a28e134dce8bfc8c91fd4e2cea74d8462700e7b6
Dessnick <dessnick@gmail.com>
2020-05-30 22:29:01 +0700

    refactoring prime game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,4 +1,5 @@
 from random import randint
+from math import sqrt
 from brain_games.cli import ask_num
 from brain_games.cli import user_answer
 GAME_RULES = ('Answer "yes" if given number is prime. '
@@ -9,12 +10,14 @@ def start(name):
     number = randint(1, 100)
     ask_num(number)
     answer = user_answer()
-    div = number - 1
-    result = 'yes'
-    while(div > 1):
-        if number % div == 0:
-            result = 'no'
-        div = div - 1
+    if number == 2:
+        result = 'yes'
+    elif number < 2:
+        result = 'no'
+    else:
+        lim = int(sqrt(number))
+        div = 2
+        result = is_not_prime(div, lim, number)
     if answer == result:
         print('Correct!')
         return True
@@ -24,3 +27,11 @@ def start(name):
               "Correct answer was " + "'{}'". format(result) + ".")
         print("Let's try again, {}!". format(name))
         return False
+
+
+def is_not_prime(div, lim, number):
+    while(div < lim):
+        if number % div == 0:
+            return 'no'
+        div += 1
+    return 'yes'
