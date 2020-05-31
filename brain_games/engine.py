1657d71d49764d41835a0aa07f61dd8d9dcc2359
Dessnick <dessnick@gmail.com>
2020-05-31 10:35:11 +0700

    refactoring engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,14 +1,21 @@
-from brain_games.cli import welcome_user
+from brain_games.cli import welcome_user, ask_user, user_answer
 
 
 def run(game):
     name = welcome_user(game.GAME_RULES)
     counter = 0
     while counter < 3:
-        correct_answer = game.start(name)
-        if correct_answer:
+        question, correct_answer = game.start()
+        ask_user(question)
+        answer = user_answer()
+        if correct_answer == answer:
+            print('Correct!')
             counter = counter + 1
         else:
+            print("'{}'". format(answer) +
+                  " is wrong answer ;(. "
+                  "Correct answer was " + "'{}'". format(correct_answer) + ".")
+            print("Let's try again, {}!". format(name))
             return
 
     print("Congratulations, {}!". format(name))
