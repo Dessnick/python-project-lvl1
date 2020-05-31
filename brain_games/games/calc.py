afccde8185ac8a7699778680393ded5628c61c1a
Dessnick <dessnick@gmail.com>
2020-05-31 10:56:20 +0700

    refactoring calc game

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,27 +1,14 @@
+from operator import add, mul, sub
 from random import randint, choice
-from brain_games.cli import ask_calc
-from brain_games.cli import user_answer
 GAME_RULES = 'What is the result of the expression?'
+OPERATOR_FUNCTION_TUPLE = [('+', add), ('*', mul), ('-', sub)]
+MAX_NUMBER = 100
 
 
-def start(name):
-    number1 = randint(1, 100)
-    number2 = randint(1, 100)
-    oper = choice('-+*')
-    ask_calc(number1, oper, number2)
-    if oper == '-':
-        result = number1 - number2
-    elif oper == '+':
-        result = number1 + number2
-    elif oper == '*':
-        result = number1 * number2
-    answer = user_answer()
-    if answer == str(result):
-        print('Correct!')
-        return True
-    else:
-        print("'{}'". format(answer) +
-              " is wrong answer ;(. "
-              "Correct answer was " + "'{}'". format(result) + ".")
-        print("Let's try again, {}!". format(name))
-        return False
+def start():
+    number1 = randint(1, MAX_NUMBER)
+    number2 = randint(1, MAX_NUMBER)
+    rand_calc = choice(OPERATOR_FUNCTION_TUPLE)
+    question = 'Question: {} {} {}'. format(number1, rand_calc[0], number2)
+    result = rand_calc[1](number1, number2)
+    return question, str(result)
